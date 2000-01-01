16c569d5874846d83a11bdd9ced19152377e5bb5
wezterm-osc52 automation <automation@ldelossa.dev>
2000-01-01 00:00:00 +0000

    wezterm-osc52 20260804-135843-0540453c-osc52.8

--- Casks/wezterm-osc52.rb
+++ Casks/wezterm-osc52.rb
@@ -1,7 +1,7 @@
 cask "wezterm-osc52" do
-  version "20260804-125826-7a001bff-osc52.7"
-  # Upstream WezTerm commit: 2bd22e73f7cb9a14f07c30f9606aa2482435cb08
-  sha256 "2c2c5abc24d11675ed3b2a4cbf48f738322d79b823499386d20f6f3e56d59c8c"
+  version "20260804-135843-0540453c-osc52.8"
+  # Upstream WezTerm commit: 41e56c899d8d5859808783e2d6a3f6681189d26e
+  sha256 "54bc7f17f310ca1619945752d5857a564aa94842222ec0ddc4456b0adb100a45"
 
   url "https://github.com/ldelossa/wezterm-osc52/releases/download/#{version}/WezTerm-macos-#{version}.zip"
   name "WezTerm"
